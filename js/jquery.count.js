d7424d36842ab7b67ae73275bf44ca22ea56599b
Duong Tran <duyenduonga1@gmail.com>
2019-06-08 15:42:14 +0700

    complete index register login page

---
+++ js/jquery.count.js
@@ -0,0 +1,7 @@
+<script src="js/jquery.counterup.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+<script>
+    jQuery(document).ready(function ($) {
+        $('.counter').counterUp
+    })
+</script>
